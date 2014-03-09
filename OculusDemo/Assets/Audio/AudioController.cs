526219b1d009f87a82eefce0e37aedf25cc86a2b
jreidy <jreidy@stanford.edu>
2014-03-09 15:22:56 -0700

    GUI and sound fx

---
+++ OculusDemo/Assets/Audio/AudioController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+	
+	public AudioSource background_music_source;
+	public AudioSource strike_source;
+
+	// Use this for initialization
+	void Start () {
+		//background_music_clip.
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
--- OculusDemo/Assets/GUI/Head_Up_Display.cs
+++ OculusDemo/Assets/GUI/Head_Up_Display.cs
@@ -5,33 +5,45 @@ public class Head_Up_Display : VRGUI
 {
 	public GUISkin skin;
 	
+	public TextMesh _round_text_mesh;
+	public GameObject _balance_cylinder;
+	public GameObject _balance_needle;
+	
+	// Interface Components
+	// Rounds
+	private int _total_rounds = 5;
+	private int _round_number = 5;
+	// Balance
+	private int _user_degree = 0;
+	private int _balance_degree = 0;
+	private int _balance_range = 30;
+	// Stamina
+	private int _user_stamina = 100;
+	
 	private int clickCount = 0;
+	
+	public void RoundNumberIs(int round_number){
+		_round_number = round_number;
+	}
+	
+	public void TotalRoundsIs(int total_rounds){
+		_total_rounds = total_rounds;
+	}
+	
+	public void UserDegreeIs(int user_degree){
+		_user_degree = user_degree;
+	}
+	
+	public void BalanceDegreeIs(int balance_degree){
+		_balance_degree = balance_degree;
+	}
+	
+	public void BalanceRangeIs(int balance_range) {
+		_balance_range = balance_range;
+	}
 	
 	public override void OnVRGUI()
 	{
-		GUI.skin = skin;
-		
-		GUILayout.BeginArea(new Rect(0f, 0f, Screen.width, Screen.height));
-		GUILayout.BeginVertical();
-		GUILayout.FlexibleSpace();
-		
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
-		if (GUILayout.Button("Click Me!"))
-		{
-			clickCount++;
-		}
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-		
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
-		GUILayout.Label("Clicks: " + clickCount);
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
-		
-		GUILayout.FlexibleSpace();
-		GUILayout.EndVertical();
-		GUILayout.EndArea();
+		_round_text_mesh.text = _round_number.ToString();
 	}
 }
