45b8a5595baf7eedd7de3cb1b5959cf96ee203d4
Tomas Forsyth Rosin <tomasrosin@gmail.com>
2014-03-11 00:58:39 -0700

    Added network communication. However with a slight error - switching cameras.

---
+++ OculusDemo/Assets/Networking/SpawnClient.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnClient : MonoBehaviour
+{
+	public Transform player;
+	
+	void OnConnectedToServer() {
+		SpawnPlayer ();
+	}
+	
+	void SpawnPlayer() {
+		Network.Instantiate (player, transform.position, transform.rotation, 0);
+	}
+	
+	void OnDisconnectedFromServer(NetworkDisconnection info) {
+		Network.RemoveRPCs (Network.player);
+		Network.DestroyPlayerObjects (Network.player);
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
+
---
+++ OculusDemo/Assets/Networking/SpawnHost.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnHost : MonoBehaviour
+{
+	public Transform player;
+	
+	void OnServerInitialized() {
+		SpawnPlayer ();
+	}
+	
+	void SpawnPlayer() {
+		Network.Instantiate (player, transform.position, transform.rotation, 0);
+	}
+	
+	void OnPlayerDisconnected(NetworkPlayer player) {
+		Network.RemoveRPCs (player);
+		Network.DestroyPlayerObjects (player);
+	}
+}
+
