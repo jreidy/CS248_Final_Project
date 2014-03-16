739142256b4063ea06dc3e87dcf18942df455e92
jreidy <jreidy@stanford.edu>
2014-03-16 15:12:12 -0700

    networking appears to work

---
+++ OculusDemo/Assets/Networking/SpawnScriptHost.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScriptHost : MonoBehaviour
+{
+	public Transform player;
+	
+	void OnServerInitialized() {
+		SpawnPlayer ();
+	}
+	
+
+	void SpawnPlayer() {
+		Network.Instantiate (player, transform.position, transform.rotation, 0);
+	}
+	
+	void OnPlayerDisconnected(NetworkPlayer player) {
+		Network.RemoveRPCs (player);
+		Network.DestroyPlayerObjects (player);
+	}
+	
+
+}
+
