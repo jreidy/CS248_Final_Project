3a93ea01edd3c0024aff79e905c220cd4d3df658
hmily <missjumping@gmail.com>
2014-03-18 22:05:06 -0700

    avatars walking with key w pressed

---
+++ OculusDemo/Assets/OVR/Scripts/Avatar1.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+ if (Input.GetKey("w")) {
+ animation["walk"].speed = 3.0;
+ 	animation.Play("walk");
+ 	} else {
+    animation.Stop("walk");
+    }
+}
